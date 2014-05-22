1214a28a1eb712da0bca4182061451170376431f
JaimeGamero <jaimegamero@hotmail.es>
2014-05-22 17:22:35 +0200

    Jaime
    Varios cambios

--- server/FacebookAuthenticatedServiceImpl.java
+++ server/FacebookAuthenticatedServiceImpl.java
@@ -4,27 +4,26 @@ package gossapp.server;
 import gossapp.client.services.FacebookAuthenticatedService;
 import gossapp.server.resources.FacebookFriendsResource;
 import gossapp.server.resources.FacebookResource;
-import gossapp.shared.domain.facebook.FaceFeed;
 import gossapp.shared.domain.facebook.FacebookFriends;
+import gossapp.shared.domain.facebook.FacebookPhoto;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 @SuppressWarnings("serial")
 public class FacebookAuthenticatedServiceImpl extends RemoteServiceServlet
 		implements FacebookAuthenticatedService {
-
-	@Override
-	public FacebookFriends findFriends(String access_token)
+	public void shareContent(String access_token, String content)
 			throws IllegalArgumentException {
-		FacebookFriendsResource fbr = new FacebookFriendsResource(access_token);
-		return fbr.getFriends();
+		FacebookResource fbr = new FacebookResource(access_token);
+		fbr.postContent(content);
 	}
 
 	@Override
-	public FaceFeed feedFacebook(String access_token)
+	public FacebookPhoto findPhoto(String access_token)
 			throws IllegalArgumentException {
-		FacebookResource fb = new FacebookResource(access_token);
-		return fb.getFeed();
+		FacebookResource fbp = new FacebookResource(access_token);
+		return fbp.getPhotos();
 	}
+
 
 }
